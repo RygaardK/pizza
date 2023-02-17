40fb333b2e9382ba01f65213977e448b825d9d71
Kristoffer Rygaard <k.rygaard@gmail.com>
2023-02-17 15:00:29 +0100

    minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
+function showMyBurger() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
   }
+}
+document.onload = showMyBurger();
